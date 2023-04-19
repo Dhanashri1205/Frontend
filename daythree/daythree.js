74b755715bb70a57479557b00c53a96fd8e3bfe1
Dhanashri1205 <dsonawane1205@gmail.com>
2023-04-19 10:33:24 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,43 @@
+/*
+alert("Congatulation!.....")
+alert("Hello...World")
+alert(4+2)
+
+//datatypes and their scope
+let num=56;//local global and block
+    alert("Let datatype: "+num)
+var num2=34.5; //local and global 
+    alert("Var datatype: "+num2)
+const num3="Neha" //local global and block
+    alert("Const datatype: "+num3)   
+    num3=2345;//const value can't change
+    alert(num3)
+
+//assignment operator
+let a=5; 
+a+=5;
+alert(a)
+
+//relational operator
+
+alert("Relational operator > : "+(7>3))
+
+//logical operator
+var a1=1;
+var a2=0;
+var res=a1|a2;
+alert("& operator: "+res);
+var res=a1&a2;
+alert("| operator: "+res);
+
+//If else
+if(89>56)
+alert("89 is greater than 56")
+else
+alert("56 is grater than 89")
+
+//typeof operator
+alert(typeof("Dhanu"))
+
+let age=prompt("What's your age: ")
+alert("Your age is : "+age );*/
---
+++ daythree/project.js
@@ -0,0 +1,6 @@
+function cal(){
+    var str=document.getElementById("uname").value;
+    var strlenngth=str.length;
+    const x=44;
+    document.getElementById("b1").innerHTML="Your scor is :"+Number(strlenngth+x);
+}
